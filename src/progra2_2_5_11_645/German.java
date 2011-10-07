d1a142a98a5a7375e0cda2aa12896fd57992368a
German Reyes <german.reyes10@gmail.com>
2011-10-07 08:57:14 -0600

    Este archivo contiene el nombre del alumno

---
+++ src/progra2_2_5_11_645/German.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra2_2_5_11_645;
+
+/**
+ *
+ * @author usuario
+ */
+public class German {
+    public static void main (String arg[]){
+    System.out.print("German Reyes");
+}
+}
+
